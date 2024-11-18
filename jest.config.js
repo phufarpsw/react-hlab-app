ee397a226ed2ecb993b5afb300092d8ae05df6fb
phufarpsw <phufa_rpsw@hotmail.com>
2024-11-18 23:57:58 +0700

    setup(react): for test

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  testEnvironment: "jsdom",
+  presets: [
+    "@babel/preset-env", // For transforming modern JS
+    "@babel/preset-react", // For transforming JSX/React code
+  ],
+};
